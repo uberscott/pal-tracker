edd8b5bc69cc5d2f14ad98c043c45ebf65bf6764
Scott Williams <paul.williams@asynchrony.com>
2018-06-06 11:50:42 -0600

    security

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -7,6 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WelcomeControllerTest {
 
+
+
+
     @Test
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
